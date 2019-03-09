b6aa0552c1ae1f6b4b36bfa3d459b47b81f60bd4
Kuek Kiang Kuang <kiangkuang@gmail.com>
2019-03-09 19:18:57 +0800

    update other domain

--- RedditDiscussionLink.9anime.user.js
+++ RedditDiscussionLink.9anime.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
-// @name         Reddit Discussion Link on 9anime.to
+// @name         Reddit Discussion Link on 9anime
 // @namespace    https://github.com/kiangkuang
-// @version      0.1
-// @description  Adds a link to Reddit discussion threads on 9anime.to video pages
+// @version      0.2
+// @description  Adds a link to Reddit discussion threads on 9anime video pages
 // @homepage     https://github.com/kiangkuang/userscripts
 // @supportURL   https://github.com/kiangkuang/userscripts/issues
 // @updateURL    https://github.com/kiangkuang/userscripts/raw/master/RedditDiscussionLink.9anime.user.js
 // @downloadURL  https://github.com/kiangkuang/userscripts/raw/master/RedditDiscussionLink.9anime.user.js
 // @author       Kiang Kuang
 // @match        *.9anime.to/watch/*
+// @match        *.9anime.ru/watch/*
 // @grant        none
 // ==/UserScript==
 
