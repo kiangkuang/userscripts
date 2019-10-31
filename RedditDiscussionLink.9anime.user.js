851959be0cd9078bc03ca6e52b1d29af7a04f6fc
Kuek Kiang Kuang <kiangkuang@gmail.com>
2019-11-01 00:12:08 +0800

    fix for links with no subdomain

--- RedditDiscussionLink.9anime.user.js
+++ RedditDiscussionLink.9anime.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Reddit Discussion Link on 9anime
 // @namespace    https://github.com/kiangkuang
-// @version      0.3
+// @version      0.4
 // @description  Adds a link to Reddit discussion threads on 9anime video pages
 // @homepage     https://github.com/kiangkuang/userscripts
 // @supportURL   https://github.com/kiangkuang/userscripts/issues
 // @updateURL    https://github.com/kiangkuang/userscripts/raw/master/RedditDiscussionLink.9anime.user.js
 // @downloadURL  https://github.com/kiangkuang/userscripts/raw/master/RedditDiscussionLink.9anime.user.js
 // @author       Kiang Kuang
-// @include      *.9anime.*/watch/*
+// @include      *9anime.*/watch/*
 // @grant        none
 // ==/UserScript==
 
