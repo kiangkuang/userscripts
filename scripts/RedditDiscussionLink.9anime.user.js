8a0c054d47d29439d1ed19db5be59e24e89dd112
Kuek Kiang Kuang <kiangkuang@gmail.com>
2021-02-09 02:28:27 +0800

    Update RedditDiscussionLink.9anime.user.js

--- scripts/RedditDiscussionLink.9anime.user.js
+++ scripts/RedditDiscussionLink.9anime.user.js
@@ -1,13 +1,19 @@
 // ==UserScript==
 // @name         Reddit Discussion Link on 9anime
 // @namespace    https://github.com/kiangkuang/userscripts
-// @version      0.9
+// @version      0.10
 // @description  Adds a link to Reddit discussion threads on 9anime video pages
 // @author       Kiang Kuang
 // @include      *9anime.*/watch/*
+// @include      https://www.google.com/url?q=*
 // ==/UserScript==
 
 (() => {
+  const redirect = 'https://www.google.com/url?q=';
+  if (window.location.href.startsWith(redirect)) {
+    window.location = window.location.href.replace(redirect, '');
+  }
+
   const $ = unsafeWindow.jQuery;
 
   const e = $('<a href="#" target="_blank" class="ctl" title="Reddit Discussion Thread" style="color: #ababab; padding: 0 8px;"><i class="fab fa-reddit"></i> <span>Reddit</span></a>');
