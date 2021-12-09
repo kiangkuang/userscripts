963c6cdee23f01e9b9da2a44dbc7b7d0fcc3ea48
Kiang Kuang <kiangkuang@gmail.com>
2021-12-09 11:18:53 +0800

    Add label background colour and longer board height

--- scripts/JiraStoryNameWidth.user.js
+++ scripts/JiraStoryNameWidth.user.js
@@ -1,17 +1,24 @@
 // ==UserScript==
 // @name         Jira Story Name Width
 // @namespace    https://github.com/kiangkuang/userscripts
-// @version      0.3
+// @version      0.4
 // @description  Removes max-width css from story display in task board
 // @author       Kiang Kuang
 // @match        https://*.atlassian.net/jira/software/projects/*/boards/*
 // ==/UserScript==
 
 (() => {
-  setInterval(() => {
-    document.querySelectorAll('[data-test-id="platform-board-kit.ui.swimlane.swimlane-content"] > div > button')
-      .forEach((x) => {
-        x.style['max-width'] = 'unset';
-      });
-  }, 1000);
+  const style = document.createElement('style');
+  style.innerHTML = `
+  [data-test-id="platform-board-kit.ui.swimlane.swimlane-content"] button {
+    max-width: unset !important;
+  }
+  [data-test-id="platform-card.ui.card.focus-container"] .css-4wyy7n {
+    background-color: gold !important;
+  }
+  [data-test-id="software-board.board"] {
+    height: unset !important;
+  }
+  `;
+  document.getElementsByTagName('head')[0].appendChild(style);
 })();
