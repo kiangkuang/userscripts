eed31ae24f24f2e08528738efa665026aa5ef6f0
Kiang Kuang <kiangkuang@gmail.com>
2022-06-23 02:42:24 +0800

    Update JiraEnhancements.user.js

--- scripts/JiraEnhancements.user.js
+++ scripts/JiraEnhancements.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Jira Enhancements
 // @namespace    https://github.com/kiangkuang/userscripts
-// @version      0.5
+// @version      0.6
 // @description  Assortment of Jira UI enhancements
 // @author       Kiang Kuang
 // @match        https://*.atlassian.net/jira/software/projects/*/boards/*
@@ -9,7 +9,7 @@
 
 (() => {
   const LABEL_BACKGROUND = 'gold';
-  const LABEL_COLOR = 'rgba(0, 0, 0, 0.7)';
+  const LABEL_COLOR = 'rgba(0, 0, 0, 0.6)';
 
   const style = document.createElement('style');
   style.innerHTML = `
@@ -17,9 +17,14 @@
     max-width: unset !important;
   }
 
-  [data-test-id="platform-card.ui.card.focus-container"] > div > div > div > div > span {
+  [data-test-id="platform-card.ui.card.focus-container"] > div > div > div > div > span,
+  [data-test-id="platform-card.ui.card.focus-container"] > div > div > div > div > span > span {
     background: ${LABEL_BACKGROUND} !important;
     color: ${LABEL_COLOR} !important;
+    font-size: 11px !important;
+    font-weight: 700 !important;
+    text-transform: uppercase !important;
+    align-items: center !important;
   }
 
   [data-test-id="software-board.board"] {
