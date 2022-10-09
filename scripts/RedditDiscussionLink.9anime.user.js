22cd146aa1a82e99bb544a1f64a58f8f5f82265c
Kiang Kuang <kiangkuang@gmail.com>
2022-10-09 14:29:33 +0800

    Update RedditDiscussionLink.9anime.user.js

--- scripts/RedditDiscussionLink.9anime.user.js
+++ scripts/RedditDiscussionLink.9anime.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Reddit Discussion Link on 9anime
 // @namespace    https://github.com/kiangkuang/userscripts
-// @version      1.0
+// @version      1.1
 // @description  Adds a link to Reddit discussion threads on 9anime video pages
 // @author       Kiang Kuang
 // @include      *9anime.*/watch/*
@@ -24,7 +24,7 @@
   e.hover(() => {
     const title = $('#w-info .title').text().replace('(TV)', '').replace(/-/g, ' ');
     const epi = $('#w-servers .tip > div:first-child > b').text();
-    const href = `https://www.google.com/search?q=site:reddit.com/r/anime ${title} ${epi} discussion`;
+    const href = `https://www.google.com/search?q=site:reddit.com/r/anime ${encodeURIComponent(title)} ${epi} discussion`;
     e.attr('href', href);
   });
 })();
